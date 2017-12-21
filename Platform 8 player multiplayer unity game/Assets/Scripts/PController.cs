400514ced944fdc1d4ab205e6dc7133f56f0868c
Moran Abecassis <moranmoa@gmail.com>
2017-12-21 18:24:16 +0200

    update

--- Platform 8 player multiplayer unity game/Assets/Scripts/PController.cs
+++ Platform 8 player multiplayer unity game/Assets/Scripts/PController.cs
@@ -15,40 +15,39 @@ public class PController : NetworkBehaviour {
 	Rigidbody rb;
 	// Use this for initialization
 	void Start () {
-		if (!isLocalPlayer)
-			Destroy (this);
 		rb = GetComponent<Rigidbody> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-	//	if (isLocalPlayer) {
-			Vector3 movement;
-			if (Input.GetKey (up)) {
-				movement = new Vector3 (-moveSpeed, 0, 0);
-			} else if (Input.GetKey (down)) {
-				movement = new Vector3 (moveSpeed, 0, 0);
-			} else if (Input.GetKey (lrft)) {
-				movement = new Vector3 (0, 0, -moveSpeed);
-			} else if (Input.GetKey (right)) {
-				movement = new Vector3 (0, 0, moveSpeed);
-			} else
-				movement = new Vector3 (0, 0, 0);
-			// = Input.GetKey ("Horizental");
-			//float VAsix = Input.GetKey ("Vertical");
-			//movement = new Vector3 (-hAsix, 0, -VAsix);
-			rb.velocity += movement;
+		if (!isLocalPlayer)
+			return;
+		Vector3 movement;
+		if (Input.GetKey (up)) {
+			movement = new Vector3 (-moveSpeed, 0, 0);
+		} else if (Input.GetKey (down)) {
+			movement = new Vector3 (moveSpeed, 0, 0);
+		} else if (Input.GetKey (lrft)) {
+			movement = new Vector3 (0, 0, -moveSpeed);
+		} else if (Input.GetKey (right)) {
+			movement = new Vector3 (0, 0, moveSpeed);
+		} else
+			movement = new Vector3 (0, 0, 0);
+		// = Input.GetKey ("Horizental");
+		//float VAsix = Input.GetKey ("Vertical");
+		//movement = new Vector3 (-hAsix, 0, -VAsix);
+		rb.velocity += movement;
 	//	}
 
 	}
-	/*
+
 	void OnCollisionEnter(Collision c)
 	{
-		// force is how forcefully we will push the player away from the enemy.
-		//if (isLocalPlayer) {
-
+		
 			// If the object we hit is the enemy
 			if (c.gameObject.tag == "Player") {
+			if (!isServer)
+				return;
 				if (sizeOfV3 (rb.velocity) > sizeOfV3 (c.gameObject.GetComponent<Rigidbody> ().velocity)) {
 					Rigidbody rbB = c.gameObject.GetComponent<Rigidbody> ();
 					Vector3 forceA = rb.mass * rb.velocity;
@@ -71,7 +70,14 @@ public class PController : NetworkBehaviour {
 	float sizeOfV3 (Vector3 v3){
 		return (v3.x * v3.x + v3.y * v3.y + v3.z * v3.z);
 	}
-*/
+
+	[Command]
+	void CmdCollisionH()
+	{
+		//GameObject ballClone = Instantiate(snowBall,throwPoint.position,throwPoint.rotation) as GameObject;
+		//ballClone.transform.localScale = scale;
+		//NetworkServer.Spawn (ballClone);
+	}
 }
 
 /*
